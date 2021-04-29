046ac1b912e75a654e7944992f34de7b70abdcfb
Grinshpon <grinshpondaniel@protonmail.com>
2021-04-28 23:58:10 -0400

    clipping

--- src/main.cpp
+++ src/main.cpp
@@ -64,6 +64,19 @@ struct Vec3 {
   }
 };
 
+// plane_n expected to be normalized
+Vec3 intersectPlane(Vec3 &plane_p, Vec3 &plane_n, Vec3 &lineStart, Vec3 &lineEnd) {
+  plane_n.normalize();
+  float plane_d = -plane_n.dot(plane_p);
+  float ad = lineStart.dot(plane_n);
+  float bd = lineEnd.dot(plane_n);
+  float t = (-plane_d-ad) / (bd-ad);
+  Vec3 startToEnd = lineEnd - lineStart;
+  Vec3 intersect = startToEnd;
+  intersect.scale(t);
+  return lineStart + intersect;
+}
+
 struct Triangle {
   std::array<Vec3,3> points;
   olc::Pixel color;
@@ -78,6 +91,72 @@ struct Triangle {
     points[1].scale(sx,sy,sz);
     points[2].scale(sx,sy,sz);
   }
+
+  Triangle copy() {
+    return {
+      .points = points,
+      .color = color
+    };
+  }
+
+  // return how many of the two output tris to use
+  int clipAgainstPlane(Vec3 plane_p, Vec3 plane_n, Triangle &out1, Triangle &out2) {
+    plane_n.normalize();
+
+    auto dist = [&](Vec3 &p) {
+      return (
+        plane_n.x*p.x + plane_n.y*p.y + plane_n.z*p.z - plane_n.dot(plane_p)
+      );
+    };
+
+    std::array<Vec3*, 3> insidePts;
+    int insidePtCount = 0;
+    std::array<Vec3*, 3> outsidePts;
+    int outsidePtCount = 0;
+
+    float d0 = dist(points[0]);
+    float d1 = dist(points[1]);
+    float d2 = dist(points[2]);
+    if (d0 >= 0) { insidePts[insidePtCount++] = &points[0];}
+    else { outsidePts[outsidePtCount++] = &points[0];}
+    if (d1 >= 0) { insidePts[insidePtCount++] = &points[1];}
+    else { outsidePts[outsidePtCount++] = &points[1];}
+    if (d2 >= 0) { insidePts[insidePtCount++] = &points[2];}
+    else { outsidePts[outsidePtCount++] = &points[2];}
+
+    if (insidePtCount == 0) {
+      // all points are outside plane, so whole triangle should be clipped
+      return 0;
+    }
+    else if (insidePtCount == 3) {
+      // all points are inside plane, so triangle should be rendered whole and not clipped
+      out1 = copy();
+      return 1;
+    }
+    else if (insidePtCount == 1 && outsidePtCount == 2) {
+      // triangle should be clipped, resulting in a smaller triangle
+      out1.color = color;
+      out1.points[0] = *(insidePts[0]);
+      out1.points[1] = intersectPlane(plane_p, plane_n, *(insidePts[0]), *(outsidePts[0]));
+      out1.points[2] = intersectPlane(plane_p, plane_n, *(insidePts[0]), *(outsidePts[1]));
+      return 1;
+    }
+    else if (insidePtCount == 2 && outsidePtCount == 1) {
+      // triangle should be clipped, resulting in quad (two tris)
+      out1.color = color;
+      out2.color = color;
+
+      out1.points[0] = *(insidePts[0]);
+      out1.points[1] = *(insidePts[1]);
+      out1.points[2] = intersectPlane(plane_p, plane_n, *(insidePts[0]), *(outsidePts[0]));
+      out2.points[0] = *(insidePts[1]);
+      out2.points[1] = out1.points[2];
+      out2.points[2] = intersectPlane(plane_p, plane_n, *(insidePts[1]), *(outsidePts[0]));
+      return 2;
+    }
+    return -1;
+  }
+
 };
 
 struct Mesh {
@@ -162,11 +241,23 @@ Vec3 mulProj(Vec3 &u, Mat4 &m) {
   return res;
 }
 
+// Pretend a 3d vector is a 4d vector with w=1, multiply by the matrix, then return the result, discarding w
+Vec3 mulV4M4(Vec3 &u, Mat4 &m) {
+  Vec3 res;
+
+  res.x = u.x * m.get(0,0) + u.y * m.get(1,0) + u.z * m.get(2,0) + m.get(3,0);
+  res.y = u.x * m.get(0,1) + u.y * m.get(1,1) + u.z * m.get(2,1) + m.get(3,1);
+  res.z = u.x * m.get(0,2) + u.y * m.get(1,2) + u.z * m.get(2,2) + m.get(3,2);
+
+  return res;
+}
+
 Triangle project(Triangle &tri, Mat4 &m) {
   Triangle res;
   res.points[0] = mulProj(tri.points[0], m);
   res.points[1] = mulProj(tri.points[1], m);
   res.points[2] = mulProj(tri.points[2], m);
+  res.color = tri.color;
   return res;
 }
 
@@ -178,7 +269,6 @@ olc::Pixel getColor(float lum) {
     return olc::WHITE * lum;
   }
 }
-
 
 Mat4 projectionMatrix(float fnear, float ffar, float fov, float aspectRatio) {
   //putting an f in the front makes an "expected qualified-id" error go away,
@@ -286,6 +376,7 @@ class Game : public olc::PixelGameEngine {
   Mat4 projMat;
   Vec3 camPos = {0};
   Vec3 camDir = {0,0,1};
+  float yaw;
 
   float theta;
 
@@ -311,6 +402,26 @@ public:
     if (GetKey(olc::Key::DOWN).bHeld) {
       camPos.y -= 8.0f * dt;
     }
+    if (GetKey(olc::Key::LEFT).bHeld) {
+      camPos.x += 8.0f * dt;
+    }
+    if (GetKey(olc::Key::RIGHT).bHeld) {
+      camPos.x -= 8.0f * dt;
+    }
+    if (GetKey(olc::Key::A).bHeld) {
+      yaw -= 2.0f * dt;
+    }
+    if (GetKey(olc::Key::D).bHeld) {
+      yaw += 2.0f * dt;
+    }
+    Vec3 moveForward = camDir;
+    moveForward.scale(8.0f * dt);
+    if (GetKey(olc::Key::W).bHeld) {
+      camPos = camPos + moveForward;
+    }
+    if (GetKey(olc::Key::S).bHeld) {
+      camPos = camPos - moveForward;
+    }
 
     // Draw
     Clear(olc::BLACK);
@@ -327,7 +438,10 @@ public:
     matWorld = matWorld * transM;
 
     Vec3 camUp = {0,1,0};
-    Vec3 camTarget = camPos + camDir;
+    Vec3 camTarget = {0,0,1};
+    Mat4 camRot = matRotY(yaw);
+    camDir = mulV4M4(camTarget, camRot);
+    camTarget = camPos + camDir;
     Mat4 camMat = matPointAt(camPos, camTarget, camUp);
     Mat4 viewMat = quickInverse(camMat);
 
@@ -335,6 +449,7 @@ public:
 
     // Calculate triangle projection and add to draw queue
     for(auto tri: meshCube.tris) {
+      //tri.translate(10,0,0);
       Triangle triProj, triTrans, triViewed; //projected and transformed triangles
 
       triTrans = project(tri, matWorld);
@@ -354,13 +469,17 @@ public:
 
         triViewed = project(triTrans, viewMat);
 
-        triProj = project(triViewed, projMat);
-        triProj.scale(-1,-1,1);
-        triProj.translate(1.0f, 1.0f, 0.0f);
-        triProj.scale(0.5f * static_cast<float>(ScreenWidth()), 0.5f * static_cast<float>(ScreenHeight()), 1.0f);
-        triProj.color = triTrans.color;
+        std::array<Triangle, 2> outTris;
+        int clippedTris = triViewed.clipAgainstPlane({0,0,2.1f}, {0,0,1.0f}, outTris[0], outTris[1]);
 
-        trisToDraw.push_back(triProj);
+        for(int i = 0; i < clippedTris; i++) {
+          triProj = project(outTris[i], projMat);
+          triProj.scale(-1,-1,1);
+          triProj.translate(1.0f, 1.0f, 0.0f);
+          triProj.scale(0.5f * static_cast<float>(ScreenWidth()), 0.5f * static_cast<float>(ScreenHeight()), 1.0f);
+
+          trisToDraw.push_back(triProj);
+        }
       }
     }
 
@@ -380,13 +499,12 @@ public:
         tri.color
       );
       // Draw wireframe
-      /*
       DrawTriangle(
         tri.points[0].x, tri.points[0].y,
         tri.points[1].x, tri.points[1].y,
         tri.points[2].x, tri.points[2].y,
         olc::BLACK
-      );*/
+      );
     }
     return true;
   }
